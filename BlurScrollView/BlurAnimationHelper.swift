c9d93068bb1bbdb52010ee92dd3648b1eef29e96
Danny <danny@Dannys-MacBook-Pro.local>
2015-09-29 17:03:39 +0300

    refactor functional

--- BlurScrollView/BlurAnimationHelper.swift
+++ BlurScrollView/BlurAnimationHelper.swift
@@ -9,6 +9,47 @@
 import Foundation
 import UIKit
 
+infix operator <^> { associativity left }
+func <^><A, B>(a: A?, f: (A -> B)?) -> B? {
+    if let a = a, let f = f {
+        return f(a)
+    }
+    else{
+        return nil
+    }
+}
+
+infix operator <*> { associativity left }
+func <*><A, B>(optionalTransform: (A -> B)?, optionalValue: A?) -> B? {
+    if let transform = optionalTransform {
+        if let value = optionalValue {
+            return transform(value)
+        }
+    }
+    return nil
+}
+
+func curry<A, B, C>(f: (A, B) -> C) -> A -> B -> C {
+    return { x in { y in f(x, y) } }
+}
+
+func uncurry<A, B, C>(f: A -> B -> C) -> (A, B) -> C {
+    return { x, y in f(x)(y) }
+}
+
+func flipOr<B, C>(b:Bool, f: (B, B) -> C) -> (B, B) -> C {
+    if(b == true){
+        return { (x, y) in f(y, x) }
+    }
+    else{
+        return { (x, y) in f(x, y) }
+    }
+}
+
+func runFirst<A, B>(f1: A, f2: B) -> A  {
+    return f1
+}
+
 
 class BlurAnimationHelper: NSObject, UITableViewDelegate {
     var tableView:UITableView
@@ -20,8 +61,9 @@ class BlurAnimationHelper: NSObject, UITableViewDelegate {
         let cellContainer = UIView.init(frame: self.tableView.frame)
         if let superview = self.tableView.superview{
             superview.addSubview(cellContainer)
+            superview.bringSubviewToFront(cellContainer)
         }
-    
+        
         return cellContainer
     }()
     
@@ -50,7 +92,7 @@ class BlurAnimationHelper: NSObject, UITableViewDelegate {
     //MARK: Gesture recognizers
     func tableOnSelect(indexPath: NSIndexPath) {
         if(tableView.cellForRowAtIndexPath(indexPath)?.frame.origin == CGPointZero){
-            setup(indexPath)
+            setupanimate(true, indexPath:indexPath)
         }
         
         scrollPosition = tableView.contentOffset
@@ -59,56 +101,54 @@ class BlurAnimationHelper: NSObject, UITableViewDelegate {
     
     func tableOnAnimate() {
         if let indexPath = tableView.indexPathForSelectedRow {
-            setup(indexPath)
+            setupanimate(false, indexPath:indexPath)
         }
     }
     
     func tap(recognize:UITapGestureRecognizer){
         if let indexPath = tableView.indexPathForSelectedRow  {
-            teardown(indexPath)
+            setupanimate(true, indexPath:indexPath)
         }
     }
     
     //MARK: setup and teardown
-    
-    func setup(indexpath:NSIndexPath) {
-        cellContainer.alpha = 1;
-        for view in  cellContainer.subviews {
-            view.removeFromSuperview()
-        }
-    
-        if let cell = tableView.cellForRowAtIndexPath(indexpath) as? BlurCell {
-            selectedCell = cell
-            
-            cellContainer.addSubview(selectedCell.contentView)
-            
-            var rect = tableView.rectForRowAtIndexPath(indexpath)
-            rect.origin.y -= tableView.contentOffset.y
-            cellContainer.frame = rect
-            
-            cell.show()
-        }
-    
+
+    func putView(fromView:UIView, toView:UIView){
+        print("\(fromView) \(toView)")
+        toView.addSubview(fromView)
+    }
+
+    func animateAlpha(fromValue:CGFloat, toValue:CGFloat){
+        cellContainer.alpha = toValue;
+        cellContainer.backgroundColor = UIColor.redColor()
+        self.blurView.alpha = fromValue
         UIView.animateWithDuration(0.4) {
-            self.blurView.alpha = 1
+            self.blurView.alpha = toValue
         }
     }
-    
-    func teardown(indexPath:NSIndexPath) {
-        UIView.animateWithDuration(0.4) {
-            self.blurView.alpha = 0
+
+    func setupanimate(teardown:Bool, indexPath:NSIndexPath){
+        if(teardown == false){
+            curry(putView) <*>  tableView.cellForRowAtIndexPath(indexPath)?.contentView <*> cellContainer
         }
+        else{
+            curry(putView) <*>  cellContainer.subviews[0] <*> tableView.cellForRowAtIndexPath(indexPath)
+        }
+
+        curry(flipOr(teardown, f: animateAlpha)) <*> 0 <*> 1
+
         
         
-        let cell = tableView.cellForRowAtIndexPath(indexPath) as? BlurCell
-        cell?.addSubview(selectedCell.contentView)
-        tableView.setContentOffset(scrollPosition, animated: true)
-        
-        tableView.deselectRowAtIndexPath(indexPath, animated: false)
-        
-        cellContainer.alpha = 0;
-        
-        cell?.hide();
+        (tableView.cellForRowAtIndexPath(indexPath) as? BlurCell) <^> { (cell:BlurCell) in
+            var rect = cell.frame
+            rect.origin.y -= self.tableView.contentOffset.y
+            self.cellContainer.frame = rect
+            
+            return cell
+        } <^> { (cell: BlurCell) in
+            let a = (teardown == true) ? cell.hide : cell.show
+            a()
+        }
     }
 }
 
